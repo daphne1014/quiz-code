2d4c9cf9ffd1a0e5ab416f1b6e73833b76e21a23
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-09-15 13:25:22 -0700

    added use-post, delete, routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,17 +4,85 @@ const { User, Score } = require('../../models');
 //get all users
 router.get('/', (req, res) => {
     User.findAll({
-      attributes: { exclude: ['password'] }
+        attributes: { exclude: ['password'] }
     })
-      .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//get user by id
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Score,
+                attributes: ['id', 'score', 'created_at']
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//create user
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//delete user
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
   });
 
-  //get user by id
-
-  //create user
-
-  module.exports = router;
+module.exports = router;
