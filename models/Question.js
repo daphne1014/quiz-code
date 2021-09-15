af00f469b26d09091c0259d335c66fa70efce8ae
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-09-14 21:53:11 -0700

    added question model

---
+++ models/Question.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Question extends Model {}
+
+Question.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          question_text:{
+              type: DataTypes.STRING,
+              allowNull: false,
+              validate: {
+                len: [1]
+              }
+          },
+          answer: {
+              type: DataTypes.BOOLEAN,
+              allowNull: false
+          }
+        },
+          {
+            sequelize,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'question'
+          }
+)
+
+module.exports = Question;
