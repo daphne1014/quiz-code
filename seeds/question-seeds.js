61b79fac7e7a092a1ea772f87513c2b7aab13ad9
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-09-16 16:21:19 -0700

    questions addition

--- seeds/question-seeds.js
+++ seeds/question-seeds.js
@@ -2,17 +2,33 @@ const { Question } = require('../models');
 
 const questionsdata = [
   {
-    question_text: 'Question 1',
-    answer: false
-  },
-  {
-    question_text: 'Question 2',
-    answer: false
-  },
-  {
-    question_text: 'Question 3',
+    question_text: 'True or False HTML is an acronym for Hypertext Markup Language',
     answer: true
-  }
+  },
+  {
+    question_text: 'True of False the file for should be named Index.html',
+    answer: false
+  },
+  {
+    question_text: 'True or False There are somatic elements that are used to create the bones of the webpage.',
+    answer: true
+  },
+  {
+    question_text: 'True or False The correct formatting for a header is <header> </header>.',
+    answer: true
+  },
+  {
+    question_text: 'True or False SEO stands for Search Engine Observation. ',
+    answer: false
+  },
+  {
+    question_text: 'True or False SEO stands for Search Engine Observation. ',
+    answer: false
+  },
+  {
+    question_text: 'True of False CSS is an acronym for Cascading Style Sheets',
+    answer: true
+  },
   
 ];
 
