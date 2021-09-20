054518b2ace568f8eb7ea86bb27853e90b0ed5b2
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-09-19 22:53:16 -0700

    added dashboard chart.js features (data still needs to be modified)

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-
+const { User, Score } = require('../models');
 const withAuth = require('../utils/auth');
+
+
 router.get('/', withAuth, (req, res) => {
-    
-    res.render('dashboard', {
-        loggedIn: req.session.loggedIn
-      });
-  });
-  module.exports = router;
+    Score.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        iclude: [
+            {
+                model: User,
+                attributes: ['id', 'username', 'email']
+            }
+        ]
+    })
+        .then(dbScoreData => {
+            res.render('dashboard', {
+                dbScoreData,
+                loggedIn: req.session.loggedIn
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
