4c8f4e2bcd3f9e8311fbf340d3283bf5a3119374
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-09-21 22:10:12 -0700

    fixed table data problem

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -3,31 +3,41 @@
 // format your data for chart.js
 
 // fetch('/api/users/')
+const user_id = document.querySelector('body').getAttribute('data-user_id');
+
+const getData = fetch(`/api/users/${user_id}`)
+    .then(res => res.json())
+    .then(res => {
+
+        let scoresArray = res.scores;
+
+        let labels = scoresArray.map((score, index) => 'Attempt: ' + (index + 1));
+        let scores = scoresArray.map(score => score.score);
+        console.table({ labels, scores });
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'My test results',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: scores,
+            }]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+
+        const chart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    });
+
+// console.log(scores)// 
 
 
 // will need to change the data and data sets so that it gets it from db
-const data = {
-    labels: ['January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June'],
-    datasets: [{
-        label: 'My test results',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
-
-const config = {
-    type: 'bar',
-    data: data,
-    options: {}
-};
-
-const chart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
